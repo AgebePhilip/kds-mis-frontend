00af0826b27003a33ebaf6c3204ef996eb093abc
AgebePhilip <philipagebe@gmail.com>
2023-01-15 15:41:53 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import * as React from 'react';
+import ScrollTop from './components/scrollTop';
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+
+import Login from './pages/Home/Login';
+import NotFound from './pages/NotFound';
+import PrivateRoute from './routes/protected';
+import { Notifications } from 'react-push-notification';
+import AdminLayout from './components/headers/Admin';
+import SignUp from './pages/signup';
+import { CookiesProvider } from 'react-cookie';
+import AboutHome from './pages/About';
+import HomeContact from './pages/Contact';
+import HomeIcon from './pages/icon'
+
+import VerifyAccount from './pages/Auth/verify';
+import Profile from './pages/Profile';
+import ChangePassword from './pages/Auth/change_password';
+import VerifyToken from './pages/Auth/verify_token';
+import { ConfirmProvider } from 'material-ui-confirm';
+import Users from './pages/Admin/Users/users';
+import EditUser from './pages/Admin/Users/editUser';
+import Subscription from './pages/Admin/Users/transactions';
+import Statistic from './pages/Admin/statistics';
+
+const App = () =>{
+  return (
+
+    <> 
+      <CookiesProvider> 
+        <ConfirmProvider>
+          <ScrollTop />
+          <Notifications position="bottom-right"/>
+          <Routes>
+            <Route exact path="/" element={<Login/>} />
+            <Route path="/login" element={ <Login/>} />
+            <Route path="/signup" element={ <SignUp/>} />
+            <Route path="/about"  element={ <AboutHome/>} />
+            <Route path="/contact" element={<HomeContact/>} />
+           
+            
+              {/** ADMIN ROUTES */}
+            <Route path="/dashboard" element={
+              
+                <AdminLayout></AdminLayout>
+            }>
+
+
+              <Route path="" element={<Statistic />} /> 
+              <Route path="profile" element={<Profile />} /> 
+              <Route path="users" element={<Users />} />
+              <Route path="users/edit" element={<EditUser />} />
+<<<<<<< HEAD
+              <Route path='about' element={<AboutHome/>}/>
+              <Route path ='icon' element={<HomeIcon/>}/>
+=======
+		  <Route path="subscriptions" element={<Subscription />} />
+>>>>>>> c8b25f3ed272e3c29d9b8e43b17f3385a918b621
+            
+            </Route>
+            <Route path="/verify" element={<VerifyAccount />} />
+            <Route path="/change_password" element={<ChangePassword />} />
+            <Route path="/verify_token" element={<VerifyToken />} />
+            
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </ConfirmProvider>
+      </CookiesProvider>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/signup.jsx
@@ -0,0 +1,192 @@
+import  React, {useEffect} from 'react';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import useUser from '../hooks/useAuth';
+import useLoader from '../hooks/useLoader';
+import useNotification from '../hooks/useNotification';
+import { useNavigate } from 'react-router-dom';
+import useRequest from '../hooks/useRequest';
+import { Repeat } from '@mui/icons-material';
+
+function Copyright(props) {
+  return (
+    <>
+    
+    <Typography variant="body2" color="text.secondary" align="center" {...props}>
+      {'Copyright © '}
+      <Link color="inherit" href="#">
+        Kaduna State, SCI MIS
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+    </>
+  );
+}
+
+const theme = createTheme();
+
+export default function SignUp() {
+
+  const setUserData = useUser(state => state.setUserData);
+  const user = useUser(state => state.user);
+  const error = useUser(state => state.error);
+  const [Loader, showLoader, HideLoader]  = useLoader()
+  const [warningNotification, successNotification] = useNotification();
+  const navigate = useNavigate()
+
+  const [doRequest] = useRequest({
+    url:'/users/signup',
+    method: 'post',
+    onSuccess: (data) => {
+      successNotification("Successfully Signed up", "Registered")
+      setUserData(data)
+
+      //Always evaluate to true
+      if(!data.isVerified){
+        navigate('/verify')
+        return
+      }
+    }
+  })
+
+
+  const handleSubmit = async (event) => {
+
+   event.preventDefault();
+   showLoader()
+   const data = new FormData(event.currentTarget);
+   console.log({
+     email: data.get('email'),
+     password: data.get('password'),
+     remember: data.get('remember'),
+   });
+
+   await doRequest({ email:data.get('email'), password:data.get('password')})
+   HideLoader()
+  };
+
+
+
+  return (
+    
+    <ThemeProvider theme={theme}>
+      {Loader}
+      <Container component="main" maxWidth="xs" style={{width:"100vw",height:'100vh',backgroundColor:"white",marginBottom:'4rem'}}>
+        
+        <Box
+        
+          sx={{
+            
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            
+          
+
+            
+            
+            
+           
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: '#f41111' }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+
+          <div style={{borderRadius:'4rem'}}>
+          
+          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 2 }}
+         
+          >
+            
+            
+          {/*<TextField
+              margin="normal"
+              required
+              fullWidth
+              id="FullName"
+              label="Full Name"
+              name="name"
+              autoComplete="name"
+              autoFocus
+              color='success'
+              
+        /> 
+        */}
+            
+             <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="number"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              color='success'
+              
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              color='success'
+            />
+
+            {/*<TextField
+              margin="normal"
+              required
+              fullWidth
+              name="  password"
+              label="Comfirm Password"
+              type="password"
+              id="Comfirm password"
+              autoComplete="current-password"
+              color='success'
+            />
+            */}
+           
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+              color="success"
+            >
+              Sign up
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link href={'/login'} variant="body2">
+                  {"already have an account? Sign in"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+          </div>
+        </Box>
+        <Copyright sx={{ mt: 4, mb: 2 }} />
+      </Container>
+    </ThemeProvider>
+    
+    
+  );
+}
